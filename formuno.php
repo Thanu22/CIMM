14b1b86ad11043bbe62179236109e3fadd908ed5
Alvarez Tania <alvareznekochan755@gmail.com>
2022-09-21 18:08:14 -0300

    iniciando repo

---
+++ formuno.php
@@ -0,0 +1,20 @@
+<?php
+	$nreclamo=$_REQUEST['numreclamo'];
+	$fechain=$_REQUEST['fechain'];
+    $horain=$_REQUEST['horain'];
+    $falla=$_REQUEST['falla'];
+    $cruce=$_REQUEST['cruce'];
+    $detalles=$_REQUEST['detalles'];
+
+	include "conexion.php";
+
+	$SQL="INSERT INTO nuevoingreso (numreclamo, fechain, horain, falla, cruce, detalles) VALUES 
+	('$nreclamo', '$fechain', '$horain', '$falla', '$cruce' , '$detalles')";
+
+	if(!$error= $con->query($SQL)){
+        echo $con->error;
+    }
+
+    //<!--El header redirige a la pagina principal-->    
+	header ("location:principal.php");
+?>
